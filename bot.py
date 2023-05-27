2edb411ac31a0b36a792061c205beaa3824930de
Patrick Yu-En Ho <pyho@cs.washington.edu>
2023-05-26 22:54:27 -0700

    bot message update

--- bot.py
+++ bot.py
@@ -13,27 +13,56 @@ client = commands.Bot(command_prefix='/', intents=intents)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-BUILDINGS = ['test', 'cse1', 'cse2', 'ode']
-
-LOCATIONS = {'test 123': [3, 4], 'cse1 111': [2, 4], 'cse2 20': [1, 1], 'ode 333': [5, 5]} # hardcoded data
+INFO_MSG = (':information_source: My name is WIP Bot, and I\'m here to help '
+            'you find new study buddies and study locations!\n'
+            ':arrow_forward: To get started, tag yourself at an existing '
+            'location using the `tag-location` command or add a new location '
+            'using the `add-location` command. Then, find your classmates!\n'
+            ':question: Questions? Use the `/help` command to learn about '
+            'all the commands!\n'
+            ':tada: Happy studying!')
+USAGE_MSGS = ['Usage: `/add-location building number` where `building` is a '
+              'building name and `number` is a room number.',
+              'Usage: `/tag-location building number` where `building` is a '
+              'building name and `number` is a room number.',
+              'Usage: `/leave building number` where `building` is a building '
+              'name and `number` is a room number. Note that you must be '
+              'holding the location role for this location.',
+              'Usage: `/list-info`',
+              'Usage: `update-noise building number value` where `building` '
+              'a building name and `number` is a room number for a location '
+              'that exists, and `value` is the current noise level (between 1 '
+              'and 5, inclusive) of that location.',
+              'Usage: `update-noise building number value` where `building` '
+              'a building name and `number` is a room number for a location '
+              'that exists, and `value` is the current \"busy-ness\" level '
+              '(between 1 and 5, inclusive) of that location.',
+              'Usage: `list-people building number` where `building` is a '
+              'building name and `number` is a room number for a location '
+              'exists']
+BUILDINGS = ['cse1', 'cse2', 'ode']
+LOCATIONS = dict()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_member_join(member):
+  embed = discord.Embed(title=f'Welcome to {ctx.message.guild.name}!',
+                        description=INFO_MSG,
+                        color=0x57F287)
+  await member.send(embed=embed)
+
 @client.command(name='add-location',
-                help=(f'Given a building code and room number, adds a new study location.\n'
-                      f'Usage: `/add-location building number` where `building` is a building '
-                      f'code and `number` is the room number.'),
+                help=(f'Given a building code and room number, adds a new '
+                      f'study location.\n'
+                      f'{USAGE_MSGS[0]}'),
                 brief='Adds a new study location')
 async def add_location(ctx, building, number):
   # validate building and number
   building = building.lower()
-  if building not in BUILDINGS:
-    await ctx.channel.send(f'Building \"{building}\" not supported. Please try one of '
-                           f'{BUILDINGS}!')
-    return
-  if not number.isnumeric():
+  if building not in BUILDINGS or not number.isnumeric():
     await add_location_error(ctx, None)
     return
 
@@ -43,8 +72,12 @@ async def add_location(ctx, building, number):
   category = get(ctx.guild.categories, name='location-channels')
   channel = get(category.text_channels, name=channel_name)
   if channel:
-    await ctx.channel.send(f'Location \"{location}\" already exists. Please use the '
-                           f'`/tag-location` command to tag yourself!')
+    embed = discord.Embed(title=':x: An error has occurred!',
+                          description=f'Location \"{location}\" already '
+                                       'exists. Please use the `/tag-location` '
+                                       'command to tag yourself!',
+                          color=0xED4245)
+    await ctx.channel.send(embed=embed)
     return
 
   # create the location channel and role if it does not already exist
@@ -52,27 +85,41 @@ async def add_location(ctx, building, number):
     ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
     ctx.guild.me: discord.PermissionOverwrite(read_messages=True)
   }
-  channel = await ctx.guild.create_text_channel(f'{channel_name}', overwrites=overwrites,
+  channel = await ctx.guild.create_text_channel(f'{channel_name}', 
+                                                overwrites=overwrites,
                                                 category=category)
-  role = await ctx.guild.create_role(name=location, colour=random.randint(0, 0xFFFFFF), hoist=True)
+  role = await ctx.guild.create_role(name=location, 
+                                     colour=random.randint(0, 0xFFFFFF),
+                                     hoist=True)
   await channel.set_permissions(role, read_messages=True, send_messages=True)
   await ctx.author.add_roles(role, reason='/tag-location command')
   LOCATIONS[location] = ['X', 'X']
-  await ctx.channel.send(f'Location {location} added!')
+
+  # Confirm the successful addition of a new location and welcome the user at
+  # the new location
+  embed = discord.Embed(title=f':white_check_mark: Location {location} added!',
+                        description=':tada: Happy studying!',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
   await channel.send(f'Welcome <@{ctx.author.id}>!')
 
 
 @add_location.error
 async def add_location_error(ctx, error):
-  await ctx.channel.send(f'Usage: `/add-location building number` where `building` is a building '
-                         f'code and `number` is the room number.')
+  info = '`building` must be one of the following: ```\n'
+  for b in BUILDINGS:
+    info += f'{b}\n'
+  info += '```'
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=f'{USAGE_MSGS[0]}\n{info}',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
 
 
 @client.command(name='tag-location',
-                help=(f'Given a building code and room number, gives you the role for that '
-                      f'location,\n'
-                      f'Usage: `/tag-location building number` where `building` is a building '
-                      f'code and `number` is the room number.'),
+                help=(f'Given a building code and room number, gives you the '
+                      f'role for that location,\n'
+                      f'{USAGE_MSGS[1]}'),
                 brief='Gives you the role for a location')
 async def tag_location(ctx, building, number):
   # validate building and number
@@ -81,62 +128,131 @@ async def tag_location(ctx, building, number):
   category = get(ctx.guild.categories, name='location-channels')
   channel = get(category.text_channels, name=channel_name)
   if not channel:
-    await ctx.channel.send(f'Location \"{location}\" does not exist. Please use the '
-                           f'`/add-location` command to add that location.')
+    await tag_location_error(ctx, None)
+    return
 
   # Check if user already has the role
   role = get(ctx.guild.roles, name=location)
   if role in ctx.author.roles:
-    await ctx.channel.send(f'You already have the role for {location}!')
+    message = 'You already have the role for that location!'
+    embed = discord.Embed(title=':x: An error occured!',
+                          description=message,
+                          color=0xED4245)
+    await ctx.channel.send(embed=embed)
     return
 
-  # Give the location role to the user
+  # Give the location role to the user and welcome the user at the new location
   await ctx.author.add_roles(role, reason='/tag-location command')
-  await ctx.channel.send(f'Location role for {location} added!')
+  embed = discord.Embed(title=f':white_check_mark: Location role for '
+                              f'{location} added!',
+                        description=':tada: Happy studying!',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
   await channel.send(f'Welcome <@{ctx.author.id}>!')
 
 
 @tag_location.error
 async def tag_location_error(ctx, error):
-  await ctx.channel.send(f'Usage: `/tag-location building number` where `building` is a building '
-                         f'code and `number` is the room number.')
+  info = 'Try tagging yourself at one of the following locations!```\n'
+  for location in LOCATIONS.keys():
+    info += f'{location}\n'
+  info += '```'
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=f'{USAGE_MSGS[1]}\n{info}',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
 
 
-@client.command(name='clear-role',
-                 help='Removes a location role',
-                 brief='Removes a location role')
-async def clear_role(ctx, building, number):
+@client.command(name='leave',
+                 help=f'Leaves a location, removing the location role for the'
+                      f'location you are currently at.\n'
+                      f'{USAGE_MSGS[2]}',
+                 brief='Removes the location role you currently hold')
+async def leave(ctx, building, number):
   location = f'{building.lower()} {number}'
   channel_name = f'{building.lower()}-{number}'
   category = get(ctx.guild.categories, name='location-channels')
   channel = get(category.text_channels, name=channel_name)
   if not channel:
-    await ctx.channel.send(f'Location \"{location}\" does not exist. Please use the '
-                           f'`/add-location` command to add that location.')
+    await leave_error(ctx, None)
+    return
   
   # Check if user has the role
   role = get(ctx.guild.roles, name=location)
   if role not in ctx.author.roles:
-    await ctx.channel.send(f'You do not have the role for {location}!')
+    await leave_error(ctx, None)
     return
   
   # Remove the role from user
   await ctx.author.remove_roles(role, reason='/clear-role command')
-  await ctx.channel.send(f'Location role for {location} removed!')
+  embed = discord.Embed(title=f':white_check_mark: Location role for '
+                              f'{location} removed!',
+                        description=':wave: See you next time!',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
   await channel.send(f'{ctx.author.name} has left!')
 
 
-@clear_role.error
-async def clear_role_error(ctx, error):
-  await ctx.channel.send('clear_role error`')
+@leave.error
+async def leave_error(ctx, error):
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=USAGE_MSGS[2],
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
+
+@client.command(name='list-info',
+                help=f'Lists noise and busy-ness levels of all study '
+                     f'locations\n'
+                     f'{USAGE_MSGS[3]}',
+                brief='Lists noise and busy-ness levels of all study locations')
+async def list_info(ctx):
+  # construct and send the info table
+  info_table = '| Location | Noise | Busy-ness |\n' \
+                 + '|----------|-------|-----------|\n'
+
+  for location in LOCATIONS:
+    location_string = location
+    location_length = len(location_string)
+    noise_level = str(LOCATIONS[location][0])
+    busyness_level = str(LOCATIONS[location][1])
+
+    # Add padding to location string
+    while location_length < 8:
+      location_string += ' '
+      location_length += 1
+
+    info_table += ('| ' + location_string + ' |   ' + noise_level +
+                   '   |     ' + busyness_level + '     |\n')
+
+  message = (f'Noise and Busy-ness Levels for each study location on a '
+             f'scale 1 (lowest) to 5 (highest).\n'
+             f'```{info_table}```'
+             f'Note that a value of \"X\" denotes missing info. Be the '
+             f'first to report noise or busyness info!')
+  embed = discord.Embed(title=':information_source: Location Info',
+                          description=message,
+                          color=0x5865F2)
+  await  ctx.channel.send(embed=embed)
 
 
-@client.command(name='update-noise')
+@list_info.error
+async def list_info_error(ctx, error):
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=USAGE_MSGS[3],
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
+
+
+@client.command(name='update-noise',
+                help=f'Updates the noise level for a location.\n'
+                     f'{USAGE_MSGS[4]}',
+                brief='Updates the noise level for a location.')
 async def update_noise(ctx, building, number, val):
   # check if the location exists
   location = f'{building.lower()} {number}'
   if location not in LOCATIONS:
-    await ctx.channel.send(f'Location {location} not found!')
+    await update_noise_error(ctx, None)
+    return
 
   # update the location's noise level
   LOCATIONS[location] = [val, LOCATIONS[location][1]]
@@ -144,16 +260,27 @@ async def update_noise(ctx, building, number, val):
 
 @update_noise.error
 async def update_noise_error(ctx, error):
-  print(error)
-  await ctx.channel.send('error!')
+  info = 'Try updating the noise info for one of the following locations!```\n'
+  for location in LOCATIONS.keys():
+    info += f'{location}\n'
+  info += '```'
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=f'{USAGE_MSGS[4]}\n'
+                                    f'{info}',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
 
 
-@client.command(name='update-busy')
+@client.command(name='update-busy',
+                help=f'Updates the busy-ness level for a location.\n'
+                     f'{USAGE_MSGS[5]}',
+                brief='Updates the noise level for a location.')
 async def update_busy(ctx, building, number, val):
   # check if the location exists
   location = f'{building.lower()} {number}'
   if location not in LOCATIONS:
-    await ctx.channel.send(f'Location {location} not found!')
+    await update_busy_error(ctx, None)
+    return
 
   # update the location's noise level
   LOCATIONS[location] = [LOCATIONS[location][0], val]
@@ -161,65 +288,57 @@ async def update_busy(ctx, building, number, val):
 
 @update_busy.error
 async def update_busy_error(ctx, error):
-  await ctx.channel.send('error!')
-
-@client.command(name='list-info',
-                help='Lists noise and busy-ness levels of all study locations',
-                brief='Lists noise and busy-ness levels of all study locations')
-async def list_info(ctx):
-    info_table = '| Location | Noise | Busy-ness |\n' \
-                 + '|----------|-------|-----------|\n'
-
-    for location in LOCATIONS:
-        location_string = location
-        location_length = len(location_string)
-        noise_level = str(LOCATIONS[location][0])
-        busyness_level = str(LOCATIONS[location][1])
-
-        # Add padding to location string
-        while location_length < 8:
-            location_string += ' '
-            location_length += 1
-
-        info_table += ('| ' + location_string + ' |   ' + noise_level
-                       + '   |     ' + busyness_level + '     |\n')
-
-    await ctx.channel.send(f'Noise and Busy-ness Levels for each study location, '
-                           f'on a scale of 1 (lowest) to 5 (highest)\n```{info_table}```')
-
-    return
+  info = ('Try updating the busy-ness info for one of the following locations!'
+          '```\n')
+  for location in LOCATIONS.keys():
+    info += f'{location}\n'
+  info += '```'
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=f'{USAGE_MSGS[5]}\n'
+                                    f'{info}',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
 
 
 @client.command(name='list-people',
-                help=(f'Given a building code and room number, lists all people at that '
-                      f'location,\n'
-                      f'Usage: `/list-people building number` where `building` is a building '
-                      f'code and `number` is the room number.'),
+                help=(f'Given a building code and room number, lists all '
+                      f'people at that location\n'
+                      f'{USAGE_MSGS[6]}'),
                 brief='Lists people at a study location')
 async def list_people(ctx, building, number):
-    # validate building and number
-    location = f'{building.lower()}-{number}'
-    category = get(ctx.guild.categories, name='location-channels')
-    channel = get(category.text_channels, name=location)
-    if not channel:
-        await ctx.channel.send(f'Location \"{location}\" does not exist.')
-        return
+  # validate building and number
+  location = f'{building.lower()} {number}'
+  channel_name = f'{building.lower()}-{number}'
+  category = get(ctx.guild.categories, name='location-channels')
+  channel = get(category.text_channels, name=channel_name)
+  if not channel:
+    await list_people_error(ctx, None)
 
-    # list users with the role building-number
-    people = f'People at location \"{location}\":\n'
-    role = get(ctx.guild.roles, name=location)
-    members = role.members
-    for member in members:
-        people += (member.name + '\n')
-
-    await ctx.channel.send(f'```{people}```')
-    return
+  # list users with the role building-number
+  people = ''
+  role = get(ctx.guild.roles, name=location)
+  members = role.members
+  for member in members:
+    people += (member.name + '\n')
+  embed = discord.Embed(title=f':information_source: People at location '
+                              f'\"{location}\"',
+                        description=f'```{people}```',
+                        color=0x5865F2)
+  await ctx.channel.send(embed=embed)
 
 
 @list_people.error
 async def list_people_error(ctx, error):
-    await ctx.channel.send(f'Usage: `/list-people building number` where `building` is a building '
-                           f'code and `number` is the room number.')
+  info = ('Try requesting the list of people at one of the following '
+          'locations!```\n')
+  for location in LOCATIONS.keys():
+    info += f'{location}\n'
+  info += '```'
+  embed = discord.Embed(title=f':x: An error occured!',
+                        description=f'{USAGE_MSGS[6]}\n'
+                                    f'{info}',
+                        color=0xED4245)
+  await ctx.channel.send(embed=embed)
 
 
 client.run(TOKEN)
